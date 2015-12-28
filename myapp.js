528196dcef06fc039b3944dcadc03ebbfcef6a10
vipulism <vipul0809@gmail.com>
2015-12-28 18:09:00 +0530

    total items

--- myapp.js
+++ myapp.js
@@ -1,13 +1,11 @@
 var app = angular.module("theApp", []);
 app.controller("myProducts", function($scope, $http) {
 
-    $http.get("frontEndProblem.json").then(function(response) {
+    $http.get("frontEndProblem.json").then(function() {
 
         $scope.sortType = 'paymentId'; // set the default sort type
         $scope.sortReverse = false; // set the default sort order
+
         
-        $scope.items = response.data;
     });
-
-
 });
